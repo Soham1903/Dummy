e21855610f19d504745afd05c20a8a7b085ec475
Soham Anand Ladgaonkar <sohamladgaonkar19@gmail.com>
2025-04-27 14:27:29 +0530

    test

--- api/hello.js
+++ api/hello.js
@@ -2,6 +2,25 @@ import dbConnect from "../lib/db.js"; // Connect to MongoDB
 import Course from "../models/courseSchema.js"; // Your Mongoose model
 
 export default async function handler(req, res) {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,POST,PUT,DELETE,OPTIONS"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    return res.status(200).end();
+  }
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   if (req.method !== "GET") {
     return res.status(405).json({ message: "Method Not Allowed" });
   }

0c8c2f21a6429b813ce541b3fbe5fd2111701678
Soham Anand Ladgaonkar <sohamladgaonkar19@gmail.com>
2025-04-27 20:58:18 +0530

    test

--- api/login.js
+++ api/login.js
@@ -10,7 +10,8 @@ export default async function login(req, res) {
   try {
     await dbConnect(); // Connect to database
 
-    const { email, password } = req.body;
+    const body = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+    const { email, password } = body;
 
     if (!email || !password) {
       return res

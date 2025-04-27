d5005b7dc551411aa079378f2867b6c60330bf3f
Soham Anand Ladgaonkar <sohamladgaonkar19@gmail.com>
2025-04-27 19:18:11 +0530

    test

--- api/updatecourse/[title].js
+++ api/updatecourse/[title].js
@@ -28,10 +28,10 @@ export default async function handler(req, res) {
   try {
     await dbConnect();
 
-    const { id } = req.query.id; // Vercel way of getting dynamic id from URL
+    const { title } = req.query; // Vercel way of getting dynamic id from URL
 
     // Find course
-    let course = await Course.findById(id);
+    const course = await Course.findOne({ title });
     if (!course) {
       return res.status(404).json({ message: "Course not found" });
     }
